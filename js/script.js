aa45a30e9a20c40cbd06ecb7e7099cab74044258
Elena <77905432@mail.ru>
2022-04-25 19:01:01 +0300

    login form implementation

---
+++ js/script.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let hipotenuza = Math.sqrt(4 ** 2 + 9 ** 2);
+
+let result = hipotenuza < 10 ? "Точка лежит в пределах окружности" : "Точка лежит за пределами окружности";
+
+alert (result);
